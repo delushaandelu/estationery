950e5c57fc025344e1f8177a1ecb635575abfd88
Delushaan <delushaan@outlook.com>
2017-03-23 19:22:23 +0530

    login page customized

--- customer/index.php
+++ customer/index.php
@@ -44,10 +44,10 @@
 							<div class="center">
 								<h1>
 									<i class="ace-icon fa fa-leaf green"></i>
-									<span class="red">Ace</span>
-									<span class="white" id="id-text2">Application</span>
+									<span class="red">e</span>
+									<span class="white" id="id-text2">Stationery</span>
 								</h1>
-								<h4 class="blue" id="id-company-text">&copy; Company Name</h4>
+								<h4 class="blue" id="id-company-text">&copy; By University of Colombo Faculty of Management</h4>
 							</div>
 
 							<div class="space-6"></div>
